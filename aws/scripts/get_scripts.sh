da6ace6a1424ae339eb18b39035f3519591c3d69
Pranav Salunke <abheyogy@gmail.com>
2021-12-17 19:04:23 +0530

    Update get_scripts.sh

--- aws/scripts/get_scripts.sh
+++ aws/scripts/get_scripts.sh
@@ -5,3 +5,6 @@
 for i in demo_app.sql get_scripts.sh mysql_setup.sh setup_demoapp.sh setup_docker.sh; do
   curl -O https://raw.githubusercontent.com/abheyogy/demo_node_app/main/aws/scripts/$i;
 done
+
+# Save script location /tmp
+echo $0 > /tmp/userdata_script
