84a51dcc3bd5dec5d5b9a4e24d15661e9e5aff4e
Pranav Salunke <abheyogy@gmail.com>
2021-12-17 18:59:52 +0530

    Add's get_scripts to get all scripts ...

---
+++ aws/scripts/get_scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Fetch all the scripts ...
+
+for i in demo_app.sql get_scripts.sh mysql_setup.sh setup_demoapp.sh setup_docker.sh; do
+  curl -O https://raw.githubusercontent.com/abheyogy/demo_node_app/main/aws/scripts/$i;
+done
